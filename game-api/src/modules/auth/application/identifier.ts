aea4be15019e5ba42ac3492c45b914d327f2baf4
Ariel Rubilar <ariel.rubilar2@gmail.com>
2026-02-17 15:48:09 -0300

    feat(api):Add UserIdentifier use case, DTOs and tests

---
+++ game-api/src/modules/auth/application/identifier.ts
@@ -0,0 +1,28 @@
+import { UserAlreadyExistsError } from '../domain/errors';
+import { UserRepository } from '../domain/repository';
+import { User } from '../domain/user';
+import { Command, Response } from './identifier.dto';
+
+class UserIdentifier {
+  constructor(private readonly repo: UserRepository) {}
+
+  async execute({ run, name }: Command): Promise<Response> {
+    const existingUser = await this.repo.FindByRun(run);
+
+    if (existingUser != null) {
+      throw new UserAlreadyExistsError();
+    }
+
+    const user = User.Create({ run, name });
+
+    await this.repo.Save(user);
+
+    return {
+      id: user.getId(),
+      run: user.getRun(),
+      name: user.getName(),
+    };
+  }
+}
+
+export { UserIdentifier };
